d16c55eae1b28ac057ba775fa0f2840dc05cfb2d
Nidim <nidim.nikitin97@yandex.ru>
2026-01-07 23:16:08 +0400

    Убрал лишнии String

--- src/main/java/Config/Config.java
+++ src/main/java/Config/Config.java
@@ -10,10 +10,6 @@ public class Config {
     public static final String SERVER_URL = properties.getProperty("SERVER_URL");
     public static final String USERNAME = properties.getProperty("USERNAME");
     public static final String PASSWORD = properties.getProperty("PASSWORD");
-    public static final String SUCCESS_REGISTER = properties.getProperty("SUCCESS_REGISTER");
-    public static final String NOT_FOUND = properties.getProperty("NOT_FOUND");
-    public static final String NOT_RIGHT_PASS = properties.getProperty("NOT_RIGHT_PASS");
-    public static final String SUCCESS_LOGOUT = properties.getProperty("SUCCESS_LOGOUT");
 
     static {
         try (InputStream stream = Config.class.getClassLoader()
