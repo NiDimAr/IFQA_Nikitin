69a7d7bf7626fe9a3df5a84ac29454a294b179bf
Nidim <nidim.nikitin97@yandex.ru>
2026-01-19 14:38:35 +0400

    Поправил ввод логина и пароля добавил ожидание, из за этого падал тест через раз.

--- src/main/java/ifellow/example/pages/LoginPage.java
+++ src/main/java/ifellow/example/pages/LoginPage.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.SelenideElement;
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 
 
@@ -16,8 +17,8 @@ public class LoginPage {
 
 
     public LoginPage login(String username, String password) {
-        usernameInput.setValue(username);
-        passwordInput.setValue(password);
+        usernameInput.shouldHave(visible).setValue(username);
+        passwordInput.shouldHave(visible).setValue(password);
         submitButton.click();
         dashboardTitle.shouldHave(text("Назначенные мне"), Duration.ofSeconds(5));
         return this;
